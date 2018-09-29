19acd7551699c1a6ee77fc5124a5cce10f739ac8
Amandus Thorsrud <ama.thorsrud@gmail.com>
2018-09-29 13:52:05 +0200

    Add default arguments for running the program from file manager

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,24 @@ fn parse_complex(s: &str) -> Option<Complexf64> {
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() != 4 {
+    let (bounds, upper_left, lower_right) = if args.len() == 1 {
+        println!("Running with default arguments: 800x800 -1.20,0.35 -1,0.20");
+        let bounds = (800, 800);
+        let upper_left = Complex::new(-1.20, 0.35);
+        let lower_right = Complex::new(-1.0, 0.20);
+
+        (bounds, upper_left, lower_right)
+    } else if args.len() != 4 {
         eprintln!("Usage: mandelbrot PIXELS UPPERLEFT LOWERRIGHT");
         eprintln!("Example: {} 800x800 -1.20,0.35 -1,0.20", args[0]);
         std::process::exit(1);
-    }
+    } else {
+        let bounds = parse_pair(&args[1], 'x').expect("error parsing image dimensions");
+        let upper_left = parse_complex(&args[2]).expect("error parsing upper left corner point");
+        let lower_right = parse_complex(&args[3]).expect("error parsing lower right corner point");
 
-    let bounds = parse_pair(&args[1], 'x').expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[2]).expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[3]).expect("error parsing lower right corner point");
+        (bounds, upper_left, lower_right)
+    };
 
     mandelbrot::run(bounds, upper_left, lower_right);
 }

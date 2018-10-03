035954dcb5f01114b9e1342b0d14a4f0179834ed
Amandus Thorsrud <ama.thorsrud@gmail.com>
2018-10-03 17:04:50 +0200

    Re-export Complex32 as well, and remove unnecessary pub keyword

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-pub mod complex;
+mod complex;
 mod member;
 mod sdl;
 
 #[cfg(test)]
 mod tests;
 
-pub use self::complex::{Complex, Complex64};
+pub use self::complex::{Complex, Complex32, Complex64};
 pub use self::sdl::start as run;
